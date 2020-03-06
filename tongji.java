a35fa905c6cb92854082bcef5d0bb4d0688abbff
tan19961221 <edz@tandongdong.local>
2020-03-06 09:33:38 +0800

    java

--- tongji.java
+++ tongji.java
@@ -6,17 +6,6 @@ public class tongji {
  
         String str = "11223451627138",strsave = "";
          
-        char[] xx = str.toCharArray();
-        Arrays.sort(xx);
-        StringBuffer sb = new StringBuffer();
-        sb.append(xx);
-        str = sb.toString();
-         
-        for(int i = 0; i<str.length(); i ++){
-            if(!strsave.equals(""+str.charAt(i))){
-                strsave = String.valueOf(str.charAt(i));
-                System.out.print(str.charAt(i)+"出现次数:");
-                System.out.println(str.lastIndexOf(str.charAt(i))-str.indexOf(str.charAt(i))+1);
             }
              
         }
