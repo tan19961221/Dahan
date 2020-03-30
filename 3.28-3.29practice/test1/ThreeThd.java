00d20250c3fcc4c2d56a303fb140a93f5b6c6a89
tan19961221 <edz@tandongdong.local>
2020-03-30 08:59:31 +0800

    线程2作业

--- 3.28-3.29practice/test1/ThreeThd.java
+++ 3.28-3.29practice/test1/ThreeThd.java
@@ -1,7 +1,6 @@
 package s4;
 
 public class ThreeThd implements Runnable {
-    private static int count = 0;
 
     public void run() {
         System.out.println(Thread.currentThread().getName());
