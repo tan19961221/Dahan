585c16ece0a4261f796203c63340c2a5f1272d54
tan19961221 <edz@tandongdong.local>
2020-04-01 09:55:30 +0800

    作业

--- 3.30practice/qianghua3/Dynamic proxy/Dog.java
+++
@@ -1,5 +0,0 @@
-package dongtai;
-
-public interface Dog {
-    void eat();
-}
--- 3.30practice/qianghua3/Dynamic proxy/WhiteDog.java
+++
@@ -1,8 +0,0 @@
-package dongtai;
-
-public class WhiteDog implements Dog{
-    @Override
-    public void eat() {
-        System.out.println("白狗吃骨头");
-    }
-}
